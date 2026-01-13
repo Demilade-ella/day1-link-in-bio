24700655a42b718edd0540bb44094c7e034c8722
Demilade-ella <emmanuellaoluwademilade@gmail.com>
2026-01-13 08:15:03 +0100

    Initial commit

---
+++ src/components/Bio.js
@@ -0,0 +1,12 @@
+
+
+function Bio() {
+    return (
+        <div className='Bio'>
+            <h1> Otunba-Marcus Oluwademilade </h1>
+            <p> Frontend Developer | Learning React ⚛️ </p>
+        </div>
+    )
+}
+
+export default Bio;

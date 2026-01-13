92ddc754dff7d559e2be694fd5d9596f4ab4393a
Oluwademilade Otunba-marcus <emmanuellaoluwademilade@gmail.com>
2026-01-13 19:24:00 +0000

    updated Style.css, Bio.js, Navbar.js, Bio.css and Navbar.css

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,27 +1,56 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import XIcon from "@mui/icons-material/X";
 import EmailIcon from "@mui/icons-material/Email";
+import "../styles/Bio.css";
 
 function Bio() {
+  const linkButtons = [
+    {
+      title: "GitHub",
+      url: "https://github.com/Demilade-ella",
+      icon: <GitHubIcon />,
+      buttonText: "My Code (GitHub)",
+      subText: "Day-to-day updates on #100DaysOfCode.",
+    },
+    {
+      title: "Twitter / X",
+      url: "https://twitter.com",
+      icon: <XIcon />,
+      buttonText: "Follow My Journey",
+      subText: "Check out my repositories and contributions.",
+    },
+    {
+      title: "Email",
+      url: "mailto:oluwademilademarcus@gmail.com",
+      icon: <EmailIcon />,
+      buttonText: "Get In Touch",
+      subText: "Open for collaborations or work inquiries.",
+    },
+  ];
+
   return (
     <div className="Bio">
       <div className="profile">
         <h1> Otunba-Marcus Oluwademilade </h1>
         <p> Frontend Developer | Learning React ⚛️ </p>
       </div>
-      <div className="links">
-        <a href="">
-          {" "}
-          <GitHubIcon />{" "}
-        </a>
-        <a href="">
-          {" "}
-          <XIcon />{" "}
-        </a>
-        <a href="mailto:oluwademilademarcus@gmail.com">
-          {" "}
-          <EmailIcon />{" "}
-        </a>
+
+      <div className="links-button">
+        {linkButtons.map((link, index) => {
+          return (
+            <a
+              key={index}
+              href={link.url}
+              className={`btn ${link.isFeatured ? "pulse-effect" : ""}`}
+              style={{ animationDelay: `${index * 0.1}s` }}
+            >
+              <div className="btn-container">
+                <span className="icons"> {link.icon} </span> {link.buttonText}
+              </div>
+              <p> {link.subText} </p>
+            </a>
+          );
+        })}
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 import React from "react";
-import Face3Icon from "@mui/icons-material/Face3";
 import BioImage from "../Assets/bio-image.jpg";
+import "../styles/Navbar.css";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="navbarTop">
-        <Face3Icon />
         <h1> Otunba's Links </h1>
       </div>
+
       <div className="img-container">
         <img src={BioImage} alt="bio-image" />
       </div>
+
+      <div className="circle"></div>
     </div>
   );
 }
